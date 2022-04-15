a5ce0ac11854b27dd1ec7412dc12c7fbdd68917e
robertocurran <rcurran@uoc.edu>
2022-04-15 15:36:03 +0100

    Añadiendo autoprefixer

---
+++ dist/index.500f190b.js
@@ -0,0 +1,2 @@
+function shiftLogo(){const t=document.getElementById("logo"),o=Math.floor(360*Math.random()),e=`hue-rotate(${String(o)}deg)`;t.style.filter=e}document.addEventListener("DOMContentLoaded",(function(){shiftLogo()}),!1);
+//# sourceMappingURL=index.500f190b.js.map
--- dist/presentacion.e55601e7.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){var t,e,n;t=document.getElementById("logo"),e=Math.floor(360*Math.random()),n="hue-rotate(".concat(String(e),"deg)"),t.style.filter=n}),!1);
-//# sourceMappingURL=presentacion.e55601e7.js.map
