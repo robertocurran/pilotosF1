a68c1ed754d7549a3b68b6a6184b70e24f731640
robertocurran <rcurran@uoc.edu>
2022-06-12 03:12:41 +0100

    Detalles de PEC2 a PEC3

---
+++ dist/presentacion.d6c10851.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){!function(){const t=document.getElementById("logo"),e=Math.floor(360*Math.random()),n=`hue-rotate(${String(e)}deg)`;t.style.filter=n}()}),!1);
+//# sourceMappingURL=presentacion.d6c10851.js.map
